fcf5f52654bc070cbb4d8edd5d4a264da895f525
nxtcya <quinn3nxt@gmail.com>
2026-03-02 18:57:33 +0700

    p01

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -3,11 +3,10 @@ import { MissionService } from './mission.service';
 
 @Controller('mission')
 export class MissionController {
-    constructor(private readonly missionservice : MissionService){}
+  constructor(private readonly missionservice: MissionService) {}
 
-        @Get('summary')
-        getSummary(){
-            return this.missionservice.getSummary()
-        }
-    
+  @Get('summary')
+  getSummary() {
+    return this.missionservice.getSummary();
+  }
 }
--- src/mission/mission.service.ts
+++ src/mission/mission.service.ts
@@ -1,31 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
-
 @Injectable()
 export class MissionService {
-    private readonly missions = [
-        { id: 1, codename: 'OPERATION_STORM', status: 'ACTIVE' },
-        { id: 2, codename: 'SILENT_SNAKE', status: 'COMPLETED' },
-        { id: 3, codename: 'RED_DAWN', status: 'FAILED' },
-        { id: 4, codename: 'BLACKOUT', status: 'ACTIVE' },
-        { id: 5, codename: 'ECHO_FALLS', status: 'COMPLETED' },
-        { id: 6, codename: 'GHOST_RIDER', status: 'COMPLETED' }
-        ];
+  private readonly missions = [
+    { id: 1, codename: 'OPERATION_STORM', status: 'ACTIVE' },
+    { id: 2, codename: 'SILENT_SNAKE', status: 'COMPLETED' },
+    { id: 3, codename: 'RED_DAWN', status: 'FAILED' },
+    { id: 4, codename: 'BLACKOUT', status: 'ACTIVE' },
+    { id: 5, codename: 'ECHO_FALLS', status: 'COMPLETED' },
+    { id: 6, codename: 'GHOST_RIDER', status: 'COMPLETED' },
+  ];
 
-    getSummary(){
-        const result = this.missions.reduce((acc,countmission) => {
+  getSummary() {
+    const result = this.missions.reduce(
+      (acc, countmission) => {
         const status = countmission.status;
 
         if (!acc[status]) {
-        acc[status] = 0;
+          acc[status] = 0;
         }
 
         acc[status]++;
         return acc;
-        }, {} as Record<string, number>);
+      },
+      {} as Record<string, number>,
+    );
 
-        return result;
-    }
-
-    }
-
+    return result;
+  }
+}
